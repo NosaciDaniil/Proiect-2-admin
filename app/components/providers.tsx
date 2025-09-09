a8026230aa69e16c46185468484319b88d37fb5a
Daniil <daniil.nosaci@gmail.com>
2025-09-09 15:00:16 +0300

    Started project, added clerk authentication, modal and form components

---
+++ app/components/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { ReactNode } from "react";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export default function Providers({ children }: ProvidersProps) {
+  return <ClerkProvider>{children}</ClerkProvider>;
+}
+
+
